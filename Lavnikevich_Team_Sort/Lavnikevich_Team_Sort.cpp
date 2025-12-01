54d6d16a41d60756bb545ba972227b96ab027c3e
polinalavnikevich <lavnikevichpo@mail.ru>
2025-12-01 19:45:33 +0300

    Добавьте файлы проекта.

---
+++ Lavnikevich_Team_Sort/Lavnikevich_Team_Sort.cpp
@@ -0,0 +1,12 @@
+﻿// Lavnikevich_Team_Sort.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <fstream>
+#include <list>
+using namespace std;
+
+int main()
+{
+    
+}
