93ccefd99c63532e70a42ccbd57b6000acb90f7a
lavnikevichpolina <lavnikevichpo@mail.ru>
2025-12-02 18:33:53 +0300

    menu, txt

--- Lavnikevich_Team_Sort/Lavnikevich_Team_Sort.cpp
+++ Lavnikevich_Team_Sort/Lavnikevich_Team_Sort.cpp
@@ -178,13 +178,140 @@ Node<T>* listInsertionSort(Node<T>* head) {
 // Даня
 
 
+// Генерация тестовых данных
+void generateDataFile(const string& filename, int type) {
+    ofstream file(filename);
+    srand(time(0));
 
+    switch (type) {
+    case 1:
+        for (int i = 0; i < 1000; i++) {
+            file << rand() % 10000 - 5000 << " ";
+        }
+        cout << "Создан файл data1.txt с 1000 целых чисел" << endl;
+        break;
+
+    case 2:
+        for (int i = 0; i < 10000; i++) {
+            file << rand() % 10000 - 5000 << " ";
+        }
+        cout << "Создан файл data2.txt с 10000 целых чисел" << endl;
+        break;
+
+    case 3:
+    {
+        for (int i = 0; i < 1000; i++) {
+            char letter = 'a' + rand() % 26;
+            file << letter << " ";
+        }
+        cout << "Создан файл data3.txt с 1000 английских букв" << endl;
+        break;
+    }
+
+    case 4:
+        for (int i = 1000; i >= 1; i--) {
+            file << i << " ";
+        }
+        cout << "Создан файл data4.txt с 1000 чисел (1000..1)" << endl;
+        break;
+
+    case 5:
+        for (int i = 0; i < 1000; i++) {
+            float val = (rand() % 10000 - 5000) / 100.0f;
+            file << val << " ";
+        }
+        cout << "Создан файл data5.txt с 1000 дробных чисел" << endl;
+        break;
+    }
+
+    file.close();
+}
+
+void showMenu() {
+    cout << "\nТЕСТИРОВАНИЕ СОРТИРОВОК НА СПИСКАХ" << endl;
+    cout << "1. 1000 целых чисел (data1.txt)" << endl;
+    cout << "2. 10000 целых чисел (data2.txt)" << endl;
+    cout << "3. 1000 букв (data3.txt)" << endl;
+    cout << "4. 1000 чисел в обратном порядке (data4.txt)" << endl;
+    cout << "5. 1000 дробных чисел (data5.txt)" << endl;
+    cout << "6. Выход" << endl;
+    cout << "Ваш выбор (1-6): ";
+}
 
 
 int main() {
     setlocale(LC_ALL, "Russian");
+    int choice;
 
+    do {
+        showMenu();
+        cin >> choice;
 
+        if (choice >= 1 && choice <= 5) {
+            string filenames[] = { "data1.txt", "data2.txt", "data3.txt", "data4.txt", "data5.txt" };
+            string descriptions[] = { "1000 целых чисел", "10000 целых чисел",
+                                    "1000 букв", "1000 чисел в обратном порядке",
+                                    "1000 дробных чисел" };
+
+            string filename = filenames[choice - 1];
+            string desc = descriptions[choice - 1];
+
+            cout << "Тест: " << desc << endl;
+
+            if (choice == 3) {
+                Node<string>* head = readFromFile<string>(filename);
+                if (!head) {
+                    cout << "Файл не найден! Данные будут сгенерированы." << endl;
+                    generateDataFile(filename, choice);
+                    head = readFromFile<string>(filename);
+                }
+                cout << "Первые 10 элементов: ";
+                printList(head);
+
+                cout << "\nРезультаты сортировок:" << endl;
+                testSort("2. Insertion Sort", listInsertionSort, head);
+
+                freeList(head);
+            }
+            else if (choice == 5) {
+                Node<float>* head = readFromFile<float>(filename);
+                if (!head) {
+                    cout << "Файл не найден! Данные будут сгенерированы." << endl;
+                    generateDataFile(filename, choice);
+                    head = readFromFile<float>(filename);
+                }
+                cout << "Первые 10 элементов: ";
+                printList(head);
+
+                cout << "\nРезультаты сортировок:" << endl;
+                testSort("2. Insertion Sort", listInsertionSort, head);
+
+                freeList(head);
+            }
+            else { // целые числа
+                Node<int>* head = readFromFile<int>(filename);
+                if (!head) {
+                    cout << "Файл не найден! Данные будут сгенерированы." << endl;
+                    generateDataFile(filename, choice);
+                    head = readFromFile<int>(filename);
+                }
+                cout << "Первые 10 элементов: ";
+                printList(head);
+
+                cout << "\nРезультаты сортировок:" << endl;
+                testSort("2. Insertion Sort", listInsertionSort, head);
+
+                freeList(head);
+            }
+        }
+        else if (choice == 6) {
+            cout << "\nВыход из программы..." << endl;
+        }
+        else {
+            cout << "Неверный выбор! Введите число от 1 до 6." << endl;
+        }
+
+    } while (choice != 6);
 
     return 0;
 }
