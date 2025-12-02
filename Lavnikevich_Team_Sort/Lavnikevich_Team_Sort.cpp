03a078640be981d63090266242cfc3460c5879fe
polinalavnikevich <lavnikevichpo@mail.ru>
2025-12-02 15:38:08 +0300

    testtime

--- Lavnikevich_Team_Sort/Lavnikevich_Team_Sort.cpp
+++ Lavnikevich_Team_Sort/Lavnikevich_Team_Sort.cpp
@@ -82,6 +82,24 @@ Node<T>* copyList(Node<T>* head) {
     return newHead;
 }
 
+// Тестирование сортировки с точным замером времени
+template <typename T>
+void testSort(const string& name, Node<T>* (*sortFunc)(Node<T>*), Node<T>* head) {
+    Node<T>* copy = copyList(head);
+
+    auto start = high_resolution_clock::now();
+    copy = sortFunc(copy);
+    auto end = high_resolution_clock::now();
+
+    auto duration = duration_cast<microseconds>(end - start);
+    double time_ms = duration.count() / 1000.0;
+
+    cout << name << ": " << time_ms << " мс";
+    cout << " (Отсортирован: " << (isSorted(copy) ? "Да" : "Нет") << ")" << endl;
+
+    freeList(copy);
+}
+
 int main() {
     setlocale(LC_ALL, "Russian");
 
